78b5505b27c0546ac1d5a55a01cff29e96506e91
unknown <anderson.smachado@al.infnet.edu.br>
2025-03-22 13:39:06 -0300

    Arquivo faltante

---
+++ 24E4-5 Projeto de Bloco Ciencia da Computacao/TP4/tp4-exercicio-4-1-tcpserver.py
@@ -0,0 +1,28 @@
+import socket
+
+
+def server_program():
+    host = socket.gethostname()
+    port = 5000  # Inicializando na porta 5000
+
+    server_socket = socket.socket()  # obtendo a instância
+    server_socket.bind((host, port))  # vinculando endereco e porta
+
+    # Definindo 2 clientes a serem ouvidos simultaneamente
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # aceitou conexao
+    print("Conexao de: " + str(address))
+    while True:
+        # Recebendo dados em pacote de ate 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print("Dado recebido: " + str(data))
+        data = 'Olá, seja bem-vindo ao servidor TCP!'
+        conn.send(data.encode())  # Envia resposta
+
+    conn.close()  # fecha conexao
+
+
+if __name__ == '__main__':
+    server_program()
